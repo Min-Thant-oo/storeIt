f107ed94b2c5232d80a11823299748a6b2dbfa11
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-11-14 18:46:08 -0600

    sidebar implemented

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constant";
 
 const getUserByEmail = async ( email:string ) => {
     const { databases } = await createAdminClient();
@@ -47,7 +48,7 @@ export const createAccount = async ({ fullName, email }: {fullName: string; emai
             {
                 fullName,
                 email,
-                avatar: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+                avatar: avatarPlaceholderUrl,
                 accountId,
             }
         )
@@ -71,4 +72,18 @@ export const verifySecret = async ({ accountId, password }: { accountId:string; 
     } catch (error) {
         
     }
+}
+
+export const getCurrentUser = async () => {
+    const { databases, account } = await createSessionClient();
+    const result = await account.get();
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectioinId,
+        [Query.equal("accountId", result.$id)],
+    );
+
+    if(user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
 }
